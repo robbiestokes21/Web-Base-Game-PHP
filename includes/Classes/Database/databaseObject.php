49a3ac2c2e34d910ac3395dceff1077b0eb2903f
Robbie Stokes <dstokesncstudio@gmail.com>
2020-03-19 21:22:25 -0400

    update files
    Signed-off-by: Robbie Stokes <dstokesncstudio@gmail.com>

--- includes/Classes/Database/databaseObject.php
+++ includes/Classes/Database/databaseObject.php
@@ -1,10 +1,15 @@
 <?php
 
-class DatabaseObject{
+class DatabaseObject
+{
     private $conn;
 
-    public function __construct($host, $user, $password, $name){
-
+    public function __construct($host, $user, $password, $name, $port)
+    {
+        $this->conn = mysqli_connect($host, $user, $password, $name, $port);
+        if (!$this->conn) {
+            throw new Ex
+            }
     }
 
 }
--- index.php
+++ index.php
@@ -3,7 +3,8 @@ require ('./includes/Classes/Database/databaseConfigs.php');
 require ('./includes/Classes/Database/databaseObject.php');
 $dbconfigs = new DBConfig();
 $dbConfig = [];
+/** @var TYPE_NAME $dbConfig */
 $dbConfig = $dbconfigs->getDBConfigs();
-$db = new DatabaseObject($dbConfig['host'], $dbConfig['user'], $dbConfig['password'], $dbConfig['dbname']);
+$db = new DatabaseObject($dbConfig['host'], $dbConfig['user'], $dbConfig['password'], $dbConfig['dbname'], $dbConfig['port']);
 
 ?>
